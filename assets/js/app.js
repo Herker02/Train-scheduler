2f401783bfa73d1178e8c1cd559565650d6732c0
HerKer02 <herker02@gmail.com>
2019-05-30 19:00:53 -0500

    init

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAAYKK793wpSvEP-gxLAVKOib1aqbd2qxM",
+    authDomain: "train-scheduler-d1448.firebaseapp.com",
+    databaseURL: "https://train-scheduler-d1448.firebaseio.com",
+    projectId: "train-scheduler-d1448",
+    storageBucket: "train-scheduler-d1448.appspot.com",
+    messagingSenderId: "1010829073857",
+    appId: "1:1010829073857:web:f6456bb1ced934cd"
+  };
+  
+  var trainName = "";
+  var destination = "";
+  var firstTrainTime = "";
+  var frequency = "";
+
+  
